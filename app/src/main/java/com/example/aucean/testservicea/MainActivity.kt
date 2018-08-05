a7cf3975eb1a41f2053867960dcb5dd52e7ad23e
aucean <l.zhenj@gmail.com>
2018-08-05 23:47:09 +0800

    增加tcp测试

--- app/src/main/java/com/example/aucean/testservicea/MainActivity.kt
+++ app/src/main/java/com/example/aucean/testservicea/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.aucean.testservicea
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.aucean.testservicea.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    fun onConnect(v: View) {
+        val intent = Intent(this, ActivityTest::class.java)
+        intent.putExtra("ip", mBinding.ipAddress)
+        intent.putExtra("port", mBinding.port)
+        startActivity(intent)
     }
 }
